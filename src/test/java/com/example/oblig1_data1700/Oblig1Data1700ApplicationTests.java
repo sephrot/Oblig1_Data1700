a2f762e4eba2b87b5b1dc417fe109ecb62a17d57
Ali Hamid <aliash12@hotmail.com>
2024-02-15 17:32:00 +0100

    Initial commit

---
+++ src/test/java/com/example/oblig1_data1700/Oblig1Data1700ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oblig1_data1700;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig1Data1700ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
